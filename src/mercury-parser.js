23aca93fdcb0445bf9ab36928e99f1853af8a9f8
Andrew Hersee <andrew@hersee.com>
2019-12-13 17:15:44 +0000

    Strip extra returns

--- src/mercury-parser.js
+++ src/mercury-parser.js
@@ -2,17 +2,30 @@ import Mercury from '@postlight/mercury-parser';
 
 import { corsSuccessResponse, corsErrorResponse, runWarm } from './utils';
 
-const mercuryParser = async ({ queryStringParameters }, context, cb) => {
+const mercuryParser = async ({ queryStringParameters }) => {
+  let result = null;
   const { url } = queryStringParameters;
-
-  const result = await Mercury.parse(url);
-
-  return cb(
-    null,
-    result
-      ? corsSuccessResponse(result)
-      : corsErrorResponse({ message: 'There was an error parsing that URL.' })
-  );
+  let { contentType } = queryStringParameters;
+  if (!contentType) {
+    contentType = 'text';
+  }
+  console.log(`Parse: ${url} , ${contentType}`);
+  try {
+    result = await Mercury.parse(url, { contentType });
+    if (contentType === 'text') {
+      const input = result.content.trim();
+      // (\s*([\n\r]|(\\n))+\s*)+
+      // console.log(`Input:\n`+JSON.stringify(input));
+      result.content = input.replace(/(\s*([\n\r]|(\\n))+\s*)+/g, '\n');
+      // console.log(`Results:\n`+result.content);
+    }
+    console.log(`Result: ${JSON.stringify(result)}`);
+  } catch (e) {
+    console.log(`Exception: ${JSON.stringify(e)}`);
+  }
+  return result
+    ? corsSuccessResponse(result)
+    : corsErrorResponse({ message: 'There was an error parsing that URL.' });
 };
 
 export default runWarm(mercuryParser);
